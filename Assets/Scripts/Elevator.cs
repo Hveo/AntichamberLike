40998961d17c13c3970569589ebdec9e1655ad4a
Johann <johann.seys@gmail.com>
2020-08-26 13:30:59 +0200

    Elevator Loop Sound Fixed + sound set to compressed in memory

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Transform[] AnchorPoints;
+
+    private Vector3 m_Velocity;
+    private int m_IndexDestination;
+    private bool m_PlayerOverride;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
