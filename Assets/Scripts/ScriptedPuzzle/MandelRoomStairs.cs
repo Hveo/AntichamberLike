d1b01db136c42c6a73e377a5596577685ddfb442
Johann <johann.seys@gmail.com>
2020-10-01 13:16:01 +0200

    Moving stairs mandel room

---
+++ Assets/Scripts/ScriptedPuzzle/MandelRoomStairs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MandelRoomStairs : MonoBehaviour
+{
+    private AnimatorSetValue animatorValueChanger;
+
+    private void Start()
+    {
+        animatorValueChanger = GetComponent<AnimatorSetValue>();   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (animatorValueChanger)
+        {
+            animatorValueChanger.boolParam = true;
+            animatorValueChanger.SetValue();
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (animatorValueChanger)
+        {
+            animatorValueChanger.boolParam = false;
+            animatorValueChanger.SetValue();
+        }
+    }
+}
