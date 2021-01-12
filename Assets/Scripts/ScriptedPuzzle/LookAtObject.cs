18fe0d93917037bd7ef9edef5d983e03bff2a3bb
j.seys <j.seys@student.isartdigital.com>
2021-01-12 14:54:42 +0100

    Generic Way To Handle LookAt Object

---
+++ Assets/Scripts/ScriptedPuzzle/LookAtObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LookAtObject : MonoBehaviour
+{
+    public GameObject Object;
+    public float LookThreshold;
+    public string StateID;
+
+    private Transform m_PlayerTransform;
+
+    private void Start()
+    {
+        GameMgr.instance.SetStateValue(StateID, 0);
+        m_PlayerTransform = GameMgr.instance.Player.transform;
+        enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        float LookPercentage = Vector3.Dot(Camera.main.transform.forward, (Object.transform.position - m_PlayerTransform.position).normalized);
+
+        if (LookPercentage > LookThreshold)
+            GameMgr.instance.SetStateValue(StateID, 0);
+        else
+            GameMgr.instance.SetStateValue(StateID, 1);
+
+    }
+}
