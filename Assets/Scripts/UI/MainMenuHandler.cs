d7f9b6320dd91a73884b4cc721a889421ef499a6
j.seys <j.seys@student.isartdigital.com>
2021-01-14 14:46:10 +0100

    MainMenu + LeanTween Integration

---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public UnityEngine.UI.Button[] Buttons;
+
+    public void EnableInput()
+    {
+        for (int i = 0; i < Buttons.Length; ++i)
+        {
+            Buttons[i].enabled = true;
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("MDR");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/UI/MenuIntro.cs
+++ Assets/Scripts/UI/MenuIntro.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class MenuIntro : MonoBehaviour
@@ -9,6 +10,9 @@ public class MenuIntro : MonoBehaviour
     public float FadeTime;
     public Image PanelImage;
     public GameObject ButtonLayout;
+    public GameObject Generator;
+    public TextMeshProUGUI[] Texts;
+    public MainMenuHandler MainMenu;
     
     Canvas m_Canvas;
     // Start is called before the first frame update
@@ -20,6 +24,8 @@ public class MenuIntro : MonoBehaviour
 
     IEnumerator FadeAndEnableMenu()
     {
+        AudioSource src = Generator.GetComponent<AudioSource>();
+
         Color transp = new Color(0.0f, 0.0f, 0.0f, 0.0f);
         float rate = 1.0f / FadeTime;
         float progress = 0.0f;
@@ -29,6 +35,50 @@ public class MenuIntro : MonoBehaviour
             PanelImage.color = Color.Lerp(Color.black, transp, progress);
             progress += rate * Time.deltaTime;
             yield return null;
+        }
+
+
+        LeanTween.value(0.0f, 10.0f, 1.0f).setEaseInOutBack().setLoopPingPong(1).setOnUpdate(setIntensity);
+        yield return new WaitForSeconds(0.5f);
+        src.Play();
+        //while (light.intensity < 20.0f)
+        //{
+        //    light.intensity = Mathf.MoveTowards(light.intensity, 20.0f, Time.deltaTime * 20.0f);
+        //    yield return null;
+        //}
+        //src.Play();
+        //while (light.intensity > 0.0)
+        //{
+        //    light.intensity = Mathf.MoveTowards(light.intensity, 0.0f, Time.deltaTime * 30.0f);
+        //    yield return null;
+        //}
+
+        GameObject cam = Camera.main.gameObject;
+        LeanTween.moveX(cam, 20, 1.5f).setDelay(1.0f);
+        LeanTween.moveY(cam, -10, 1.5f).setDelay(1.0f);
+        LeanTween.moveZ(cam, -20, 1.5f).setDelay(1.0f);
+
+        yield return new WaitForSeconds(2.0f);
+
+        for (int i = 0; i < Texts.Length; ++i)
+        {
+            RectTransform rect = Texts[i].GetComponent<RectTransform>();
+            LeanTween.moveX(rect, 0.0f, 1.0f).setFrom(100.0f);
+        }
+        LeanTween.value(0.0f, 1.0f, 1.0f).setOnUpdate(setAlpha).setOnComplete(MainMenu.EnableInput);
+    }
+
+    public void setIntensity(float intensity)
+    {
+        Generator.GetComponent<Light>().intensity = intensity;
+    }
+
+    public void setAlpha(float a)
+    {
+        Color col = Texts[0].color;
+        for (int i = 0; i < Texts.Length; ++i)
+        {
+            Texts[i].color = new Color(col.r, col.g, col.b, a);
         }
     }
 }
