54d1c08db3669f9a773fb10e3264f02b34ab60e0
j.seys <j.seys@student.isartdigital.com>
2021-06-05 16:35:07 +0200

    Changing Folder for player prefs

--- Assets/Scripts/GameSystems/Core.cs
+++ Assets/Scripts/GameSystems/Core.cs
@@ -10,6 +10,8 @@ public class Core : MonoBehaviour
     public BuiltInGameResources BuiltInResources;
     public PlayerPrefsObject PlayerPrefs;
 
+    string m_PlayerPrefsPath;
+
     void Awake()
     {
         if (instance is null)
@@ -28,24 +30,34 @@ public class Core : MonoBehaviour
 
     void LoadPlayerPrefs()
     {
-        string FilePath = Application.persistentDataPath + "/PlayerPrefs.json";
+#if UNITY_STANDALONE_LINUX || UNITY_IOS
+        m_PlayerPrefsPath = Application.persistentDataPath + "/Overthink";
+#elif UNITY_STANDALONE_OSX
+        m_PlayerPrefsPath = "~/Library/Application Support" + "/Overthink"
+#else
+        m_PlayerPrefsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/My Games/Overthink";
 
-        if (!File.Exists(FilePath))
+#endif
+        if (!Directory.Exists(m_PlayerPrefsPath))
+            Directory.CreateDirectory(m_PlayerPrefsPath);
+
+        m_PlayerPrefsPath += "/PlayerPrefs.json";
+
+        if (!File.Exists(m_PlayerPrefsPath))
         {
             string Json = JsonUtility.ToJson(PlayerPrefs);
-            File.WriteAllText(FilePath, Json);
+            File.WriteAllText(m_PlayerPrefsPath, Json);
         }
         else
         {
-            string Json = File.ReadAllText(FilePath);
+            string Json = File.ReadAllText(m_PlayerPrefsPath);
             JsonUtility.FromJsonOverwrite(Json, PlayerPrefs);
         }
     }
 
     public void SavePlayerPrefs()
     {
-        string FilePath = Application.persistentDataPath + "/PlayerPrefs.json";
         string Json = JsonUtility.ToJson(PlayerPrefs);
-        File.WriteAllText(FilePath, Json);
+        File.WriteAllText(m_PlayerPrefsPath, Json);
     }
 }
