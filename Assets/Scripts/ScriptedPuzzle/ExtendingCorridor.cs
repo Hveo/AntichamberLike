af0c183a283756543eeb1334b2476f269dfdabfb
j.seys <j.seys@student.isartdigital.com>
2021-07-17 15:30:17 +0200

    Color gradient when walking through never ending corridor

--- Assets/Scripts/ScriptedPuzzle/ExtendingCorridor.cs
+++ Assets/Scripts/ScriptedPuzzle/ExtendingCorridor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ExtendingCorridor : MonoBehaviour
 {
+    public Gradient Gradient;
+    public Renderer[] Renderers;
     private void OnTriggerStay(Collider other)
     {
         float forwardVelocity = Vector3.Dot(LevelMgr.instance.Player.PlayerBody.velocity, LevelMgr.instance.Player.transform.forward);
@@ -11,5 +13,19 @@ public class ExtendingCorridor : MonoBehaviour
         //Vector3.Dot(GameMgr.instance.Player.PlayerBody.velocity.z)
         float newScale = Mathf.Clamp((transform.parent.localScale.z + (relativeOrientation * forwardVelocity * Time.deltaTime) * 0.07f), 1.0f, 8.0f);
         transform.parent.localScale = new Vector3(1, 1, newScale);
+        AdaptCorridorColorToCurrentLength(newScale);
+    }
+
+    void AdaptCorridorColorToCurrentLength(float currentScale)
+    {
+        float normalized = (currentScale - 1.0f) / 7.0f;
+        Color col = Gradient.Evaluate(normalized);
+
+        for (int i = 0; i < Renderers.Length; ++i)
+        {
+            Material mat = Renderers[i].material;
+            mat.SetColor("_EmissionColor", col);
+            mat.SetColor("_Color", col);
+        }
     }
 }
